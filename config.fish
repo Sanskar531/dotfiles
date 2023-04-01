ed7120dbdde0241e7515f75a00deb8c661434fc3
Sanskar531 <sanskargauchanb4@gmail.com>
2023-04-01 13:45:07 +1100

    Fix keybind for A for cmd vi mode

---
+++ config.fish
@@ -0,0 +1,7 @@
+set -g fish_vi_key_bindings
+bind -M insert jj 'set fish_bind_mode default; commandline -f backward-char force-repaint'
+bind -M insert \cn accept-autosuggestion
+bind -M default \cn accept-autosuggestion
+set fish_greeting
+bind -M default A 'set fish_bind_mode insert; commandline -f end-of-buffer'
+zoxide init fish | source
