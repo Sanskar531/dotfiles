42ffe64d6c3fa36c3d66eda8a443692b27ec76c8
Sanskar Gauchan <sanskargauchanb4@gmail.com>
2023-08-12 23:16:48 +1000

    Add support for MacOS and Linux configs and add starship.toml

--- refetch.fish
+++ refetch.fish
@@ -1,11 +1,20 @@
-rm -rf ./nvim
-rm -rf ./picom
-rm -rf ./polybar
-rm ./xmonad/xmonad.hs
-rm config.fish
+switch (uname)
+  case Linux
+    # Remove all existing configs
+    rm -rf ./picom
+    rm -rf ./polybar
+    rm ./xmonad/xmonad.hs
+    rm config.fish
 
-cp -r ~/.config/nvim/ .
-cp -r ~/.xmonad/xmonad.hs ./xmonad/
-cp -r ~/.config/polybar/ .
-cp -r ~/.config/fish/config.fish .
-cp -r ~/.config/picom .
+    # Fetch new configs
+    cp -r ~/.xmonad/xmonad.hs ./xmonad/
+    cp -r ~/.config/polybar/ .
+    cp -r ~/.config/fish/config.fish .
+    cp -r ~/.config/picom .
+end
+
+# Always refetch neovim configs
+rm -rf ./nvim
+cp -r ~/.config/nvim/ ./nvim
+rm ./starship.toml
+cp ~/.config/starship.toml .
