d86d49fa9e58853b1015efdcfd5a1a48179a7cf7
Sanskar Gauchan <sanskargauchanb4@gmail.com>
2023-08-13 00:00:58 +1000

    update fish configs for supporting mac and linux configs

--- config.fish
+++ config.fish
@@ -1,11 +1,23 @@
-set -g fish_vi_key_bindings
+# Path variables for unix
+switch (uname)
+  case Linux
+    set PATH $PATH:/home/`echo $USER`/.yarn/bin
+    set PATH $PATH:/usr/local/go/bin
+    set PATH $PATH:/home/`echo $USER`/go/bin
+  case Darwin
+    # Update path for fish
+    # Bins are installed using brew for mac os configs
+    set PATH $PATH:/opt/homebrew/bin
+end
+
+# Start zoxide and starship
+zoxide init fish | source
+starship init fish | source
+
+# Setup fish bindings
+fish_vi_key_bindings
 bind -M insert jj 'set fish_bind_mode default; commandline -f backward-char force-repaint'
 bind -M insert \cn accept-autosuggestion
 bind -M default \cn accept-autosuggestion
 set fish_greeting
 bind -M default A 'set fish_bind_mode insert; commandline -f end-of-buffer'
-zoxide init fish | source
-
-set PATH $PATH:/home/sanskar/.yarn/bin
-set PATH $PATH:/usr/local/go/bin
-set PATH $PATH:/home/sanskar/go/bin
