d9cd27395d002ec8190f1b84d18293c35edf9ea2
Sanskar Gauchan <sanskargauchanb4@gmail.com>
2023-12-10 20:01:16 +1100

    Use ansible playbooks to bootstrap a new system

--- bootstrap.fish
+++ bootstrap.fish
@@ -1,11 +1,9 @@
 switch (uname)
   case Linux
     pacman -Syyu
-    pacman -S neovim scrot feh git zoxide exa ripgrep git fd neofetch tmux jq yq ffmpeg fzf
+    pacman -S ansible
   case Darwin
-    brew install neovim zoxide exa ripgrep git fd tmux jq yq ffmpeg fzf
+    brew install ansible
 end
 
-git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-fish ./apply.fish
-nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+ansible-playbook -vv -i ./hosts.ini ./dotfiles.yml
