edf6609710a68c8a523b3bb1c5786d16eb507b17
Sanskar Gauchan <sanskargauchanb4@gmail.com>
2023-12-10 12:50:40 +1100

    Fix bootstrap script

--- bootstrap.fish
+++ bootstrap.fish
@@ -1,3 +1,11 @@
+switch (uname)
+  case Linux
+    pacman -Syyu
+    pacman -S neovim scrot feh git zoxide exa ripgrep git
+  case Darwin
+    brew install neovim zoxide exa ripgrep git
+end
 
-pacman -Syyu
-pacman -S neovim scrot feh git zoxide exa ripgrep
+git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+fish ./apply.fish
+nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
